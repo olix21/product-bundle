c0c7544dde10b78bc914abde9d3b7ada802812f5
Olivier Delbruyere <olivier.delbruyere@hotmail.com>
2017-04-17 11:09:34 +0200

    psr in baseProductType

--- Form/BaseProductType.php
+++ Form/BaseProductType.php
@@ -20,77 +20,79 @@ class BaseProductType extends AbstractType
 {
     /**
      * @param FormBuilderInterface $builder
-     * @param array $options
+     * @param array                $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $stateChoices = array();
+        $stateChoices = [];
 
-        foreach (Product::getConstantList() as $constant)
-            if(strpos($constant, 'state.'))
+        foreach (Product::getConstantList() as $constant) {
+            if (strpos($constant, 'state.')) {
                 $stateChoices[$constant] = strtolower($constant);
+            }
+        }
 
         $builder
             ->add('name')
-            ->add('brand', EntityType::class, array(
-                'required' => false,
-                'class' => 'DyweeProductBundle:Brand',
+            ->add('brand', EntityType::class, [
+                'required'     => false,
+                'class'        => 'DyweeProductBundle:Brand',
                 'choice_label' => 'name'
-            ))
-            ->add('price', MoneyType::class, array('required' => false))
-            ->add('isPriceTTC', CheckboxType::class, array('required' => false, 'label' => 'Prix TTC'))
-            ->add('seo', SeoType::class, array(
+            ])
+            ->add('price', MoneyType::class, ['required' => false])
+            ->add('isPriceTTC', CheckboxType::class, ['required' => false, 'label' => 'Prix TTC'])
+            ->add('seo', SeoType::class, [
                 'data_class' => 'Dywee\ProductBundle\Entity\BaseProduct'
-            ))
-            ->add('shortDescription', CKEditorType::class, array('required' => false))
-            ->add('mediumDescription', CKEditorType::class, array('required' => false))
-            ->add('longDescription', CKEditorType::class, array('required' => false))
-            ->add('length', NumberType::class, array('required' => false))
-            ->add('width', NumberType::class, array('required' => false))
-            ->add('height', NumberType::class, array('required' => false))
-            ->add('weight', NumberType::class, array('required' => false))
-            ->add('stock', NumberType::class, array('required' => false))
-            ->add('state', ChoiceType::class, array('choices' => $stateChoices))
-            ->add('categories', EntityType::class, array(
-                'required' => false,
-                'class' => 'DyweeProductBundle:Category',
-                'choice_label' => 'indentedName',
-                'multiple' => true,
-                'expanded' => true,
+            ])
+            ->add('shortDescription', CKEditorType::class, ['required' => false])
+            ->add('mediumDescription', CKEditorType::class, ['required' => false])
+            ->add('longDescription', CKEditorType::class, ['required' => false])
+            ->add('length', NumberType::class, ['required' => false])
+            ->add('width', NumberType::class, ['required' => false])
+            ->add('height', NumberType::class, ['required' => false])
+            ->add('weight', NumberType::class, ['required' => false])
+            ->add('stock', NumberType::class, ['required' => false])
+            ->add('state', ChoiceType::class, ['choices' => $stateChoices])
+            ->add('categories', EntityType::class, [
+                'required'      => false,
+                'class'         => 'DyweeProductBundle:Category',
+                'choice_label'  => 'indentedName',
+                'multiple'      => true,
+                'expanded'      => true,
                 'query_builder' => function ($er) {
                     return $er->createQueryBuilder('c')
                         ->orderBy('c.root', 'ASC')
                         ->addOrderBy('c.lft', 'ASC');
                 }
-            ))
-            ->add('features', CollectionType::class, array(
-                'entry_type' => FeatureElementType::class,
-                'allow_add' => true,
+            ])
+            ->add('features', CollectionType::class, [
+                'entry_type'   => FeatureElementType::class,
+                'allow_add'    => true,
                 'allow_delete' => true,
                 'by_reference' => false
-            ))
-            ->add('pictures', CollectionType::class, array(
-                'entry_type' => ProductPictureType::class,
-                'allow_add' => true,
+            ])
+            ->add('pictures', CollectionType::class, [
+                'entry_type'   => ProductPictureType::class,
+                'allow_add'    => true,
                 'allow_delete' => true,
                 'by_reference' => false
-            ))
-            ->add('promotions', CollectionType::class, array(
-                'entry_type' => PromotionType::class,
-                'allow_add' => true,
+            ])
+            ->add('promotions', CollectionType::class, [
+                'entry_type'   => PromotionType::class,
+                'allow_add'    => true,
                 'allow_delete' => true,
                 'by_reference' => false
-            ))
+            ])
             /*->add('tags', TagType::class, array(
                 'type' => 'product'
             ))*/
-            ->add('relatedProducts', EntityType::class, array(
-                'required' => false,
-                'class' => 'DyweeProductBundle:Product',
+            ->add('relatedProducts', EntityType::class, [
+                'required'     => false,
+                'class'        => 'DyweeProductBundle:Product',
                 'choice_label' => 'name',
-                'multiple' => true,
-                'expanded' => true
-            ));
+                'multiple'     => true,
+                'expanded'     => true
+            ]);
     }
 
     /**
@@ -98,8 +100,8 @@ class BaseProductType extends AbstractType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        $resolver->setDefaults(array(
+        $resolver->setDefaults([
             'data_class' => 'Dywee\ProductBundle\Entity\BaseProduct'
-        ));
+        ]);
     }
 }
